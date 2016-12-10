f801a99544652b673414c6d7f66521407afcd8e7
Simon Wallstrom <simon.wallstrom@telavox.se>
2016-12-10 18:23:01 +0100

    Day 3 problem 2

--- day03/3_1.py
+++ day03/3_1.py
@@ -1,6 +1,3 @@
-def is_triangle(s1, s2, s3):
-    return ((s1 + s2) > s3) and ((s1 + s3) > s2) and ((s2 + s3) > s1)
-
 f = open("input_3")
 count = 0
 for line in f:
---
+++ day03/3_2.py
@@ -0,0 +1,12 @@
+f = open("input_3")
+count = 0
+num = []
+for line in f:
+    num.append([int(i) for i in line.split()])
+
+for i in range(0, len(num), 3):
+    for j in range(0, 3):
+        if ((num[i][j] + num[i +1][j]) > num[i + 2][j]) and ((num[i][j] + num[i + 2][j]) > num[i + 1][j]) and ((num[i + 1][j] + num[i + 2][j]) > num[i][j]):
+            count += 1
+
+print(count)
