d4832c091c9aa18db3a55650f27f7751b6c691b4
Simon Wallstrom <simon.wallstrom@telavox.se>
2016-12-10 15:50:11 +0100

    Day01_1
    First days first problem in python

---
+++ day01/1_1.py
@@ -0,0 +1,24 @@
+f = open('input_1_1', 'r')
+moves = f.read().split(",")
+x = 0
+y = 0
+direction = 0
+
+for move in moves:
+    move = move.strip()
+    turn = move[0]
+    steps = int(move[1:])
+    if turn == 'R':
+        direction = (direction + 1) % 4
+    else:
+        direction = (direction - 1) % 4
+    if direction == 0:
+        y += steps
+    elif direction == 1:
+        x += steps
+    elif direction == 2:
+        y -= steps
+    elif direction == 3:
+        x -= steps
+
+print("Steps needed: ", abs(x)+abs(y))
