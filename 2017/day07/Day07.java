276a331d650853f5fc6f813dbee2abd1d8e872db
sWallstrom <wallstrom.simon@gmail.com>
2017-12-13 21:39:56 +0100

    tempelate and start of day07

---
+++ 2017/day07/Day07.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Day03 {
+    public static void main(String[] args) {
+        List<XX> input = Arrays.asList(
+            1,s,3,4,f
+        );
+
+        System.out.println("Hello world");
+    }
+}
