5ca542afcaac9ddb4eee25ef002646ef5e75cc4b
Simon Wallstrom <simon.wallstrom@telavox.se>
2018-12-06 07:05:10 +0100

    day06 done

--- 2018/day06/Day06.java
+++ 2018/day06/Day06.java
@@ -18,25 +18,90 @@ public class Day06 {
         System.out.println("one: " + (one - start) + "ms two: " + (two - one) + "ms");
     }
 
-    private String part1(List<String> input) {
-        return "part1";
+    private int part1(List<Coordinate> input) {
+        int minX = input.stream().mapToInt(c -> c.x).min().getAsInt();
+        int maxX = input.stream().mapToInt(c -> c.x).max().getAsInt();
+        int minY = input.stream().mapToInt(c -> c.y).min().getAsInt();
+        int maxY = input.stream().mapToInt(c -> c.y).max().getAsInt();
+
+        for (int i = minX; i <= maxX; i++) {
+            for (int j = minY; j <= maxY; j++) {
+                Coordinate tmpC = null;
+                int minDistance = Integer.MAX_VALUE;
+                // min dis between i,j and c.x, c.y
+                for (Coordinate coordinate : input) {
+                    int distance = Math.abs(coordinate.x - i) + Math.abs(coordinate.y - j);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        tmpC = coordinate;
+                    }
+                    if (coordinate.x == minX || coordinate.x == maxX || coordinate.y == minY || coordinate.y == maxY) {
+                        coordinate.edge = true; // hm, only do this once? even remove
+                    }
+                }
+                tmpC.manhattanArea++;
+            }
+        }
+
+        return input.stream().filter(coordinate -> !coordinate.edge).mapToInt(coordinate -> coordinate.manhattanArea).max().getAsInt();
     }
 
-    private String part2(List<String> input) {
-        return "part1";
+    private int part2(List<Coordinate> input) {
+        int minX = input.stream().mapToInt(c -> c.x).min().getAsInt();
+        int maxX = input.stream().mapToInt(c -> c.x).max().getAsInt();
+        int minY = input.stream().mapToInt(c -> c.y).min().getAsInt();
+        int maxY = input.stream().mapToInt(c -> c.y).max().getAsInt();
+
+        int[][] grid = new int[maxX+1][maxY+1];
+
+        for (int i = minX; i <= maxX; i++) {
+            for (int j = minY; j <= maxY; j++) {
+                for (Coordinate coordinate : input) {
+                    int distance = Math.abs(coordinate.x - i) + Math.abs(coordinate.y - j);
+                    grid[i][j] += distance;
+                }
+            }
+        }
+
+        int regionSize = 0;
+        for (int i = minX; i <= maxX; i++) {
+            for (int j = minY; j <= maxY; j++) {
+                if (grid[i][j] < 10000) {
+                    regionSize++;
+                }
+            }
+        }
+
+
+        return regionSize;
     }
 
-    private List<String> init(){
+    private List<Coordinate> init(){
         String path = "input.txt";
         if (System.getProperty("user.dir").endsWith("adventofcode")) { // executed from the proj root dir
             path = "tmp/production/adventofcode/" + this.getClass().getSimpleName().toLowerCase() + "/" + path;
         }
 
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.forEach(System.out::println);
+            return stream.map(Coordinate::new).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
         return Collections.emptyList();
     }
+
+    private class Coordinate {
+        int x;
+        int y;
+        int manhattanArea;
+        boolean edge;
+
+        public Coordinate(String line) {
+            String[] split = line.split(", ");
+            x = Integer.parseInt(split[0]);
+            y = Integer.parseInt(split[1]);
+        }
+    }
+
+
 }
