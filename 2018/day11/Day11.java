5708d7f6f7c03ec024a30dd96f9ea46b546242c3
Simon Wallstrom <simon.wallstrom@telavox.se>
2018-12-11 07:49:17 +0100

    init day11

---
+++ 2018/day11/Day11.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.stream.*;
+import java.nio.file.*;
+import java.io.*;
+
+public class Day11 {
+    public static void main(String[] args) {
+        Day11 day = new Day11();
+    }
+
+    public Day11() {
+        var input = init();
+        long start = System.currentTimeMillis();
+        System.out.println("Part one: " + part1(input));
+        long one = System.currentTimeMillis();
+        System.out.println("Part two: " + part2(input));
+        long two = System.currentTimeMillis();
+        System.out.println("one: " + (one - start) + "ms two: " + (two - one) + "ms");
+    }
+
+    private String part1(List<String> input) {
+        return "part1";
+    }
+
+    private String part2(List<String> input) {
+        return "part1";
+    }
+
+    private List<String> init(){
+        String path = "input.txt";
+        if (System.getProperty("user.dir").endsWith("adventofcode")) { // executed from the proj root dir
+            path = "tmp/production/adventofcode/" + this.getClass().getSimpleName().toLowerCase() + "/" + path;
+        }
+
+        try (Stream<String> stream = Files.lines(Paths.get(path))) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+}
