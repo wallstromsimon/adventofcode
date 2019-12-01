7e3ae2f76643dab3c1663b0ccd41ad8867554627
Simon Wallstrom <simon.wallstrom@telavox.se>
2019-12-01 10:12:19 +0100

    init day01

--- 2018/day01/Day01.java
+++ 2018/day01/Day01.java
@@ -1,12 +1,7 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.*;
+import java.nio.file.*;
+import java.io.*;
 
 public class Day01 {
     public static void main(String[] args) {
@@ -14,31 +9,31 @@ public class Day01 {
     }
 
     public Day01() {
-        List<Integer> frequencyChanges = init();
-        System.out.println("Part one: " + frequencyChanges.stream().mapToInt(Integer::intValue).sum());
-
-        Set<Integer> frequencies = new HashSet<>();
-        int sum = 0;
-        for (int i = 0;; i = (i+1) % frequencyChanges.size()) {
-            Integer freq = frequencyChanges.get(i);
-            sum += freq;
-            if (frequencies.contains(sum)) {
-               System.out.println("Part two: " + sum);
-               break;
-            } else {
-                frequencies.add(sum);
-            }
-        }
+        var input = init();
+        long start = System.currentTimeMillis();
+        System.out.println("Part one: " + part1(input));
+        long one = System.currentTimeMillis();
+        System.out.println("Part two: " + part2(input));
+        long two = System.currentTimeMillis();
+        System.out.println("one: " + (one - start) + "ms two: " + (two - one) + "ms");
     }
-    
-    private List<Integer> init(){ // inherit instead? :O
+
+    private String part1(List<String> input) {
+        return "part1";
+    }
+
+    private String part2(List<String> input) {
+        return "part2";
+    }
+
+    private List<String> init(){
         String path = "input.txt";
         if (System.getProperty("user.dir").endsWith("adventofcode")) { // executed from the proj root dir
             path = "tmp/production/adventofcode/" + this.getClass().getSimpleName().toLowerCase() + "/" + path;
         }
 
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            return stream.map(Integer::parseInt).collect(Collectors.toList());
+            stream.forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
