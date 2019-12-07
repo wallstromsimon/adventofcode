bb8a767f35f013ced6b87158666c2f03646c925c
Simon Kihlberg Wallström <wallstrom.simon@gmail.com>
2019-12-07 23:18:02 +0100

    d5 start

--- 2019/day05/Day05.java
+++ 2019/day05/Day05.java
@@ -1,7 +1,11 @@
-import java.util.*;
-import java.util.stream.*;
-import java.nio.file.*;
-import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day05 {
     public static void main(String[] args) {
@@ -9,35 +13,77 @@ public class Day05 {
     }
 
     public Day05() {
-        var input = init();
+        String input = "3,225,1,225,6,6,1100,1,238,225,104,0,2,171,209,224,1001,224,-1040,224,4,224,102,8,223,223," +
+                "1001,224,4,224,1,223,224,223,102,65,102,224,101,-3575,224,224,4,224,102,8,223,223,101,2,224,224,1," +
+                "223,224,223,1102,9,82,224,1001,224,-738,224,4,224,102,8,223,223,1001,224,2,224,1,223,224,223,1101," +
+                "52,13,224,1001,224,-65,224,4,224,1002,223,8,223,1001,224,6,224,1,223,224,223,1102,82,55,225,1001," +
+                "213,67,224,1001,224,-126,224,4,224,102,8,223,223,1001,224,7,224,1,223,224,223,1,217,202,224,1001," +
+                "224,-68,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1002,176,17,224,101,-595,224,224,4," +
+                "224,102,8,223,223,101,2,224,224,1,224,223,223,1102,20,92,225,1102,80,35,225,101,21,205,224,1001," +
+                "224,-84,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1101,91,45,225,1102,63,5,225,1101," +
+                "52,58,225,1102,59,63,225,1101,23,14,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999," +
+                "1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105," +
+                "1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0," +
+                "0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999," +
+                "1008,677,677,224,1002,223,2,223,1006,224,329,101,1,223,223,1108,226,677,224,1002,223,2,223,1006," +
+                "224,344,101,1,223,223,7,677,226,224,102,2,223,223,1006,224,359,1001,223,1,223,8,677,226,224,102," +
+                "2,223,223,1005,224,374,1001,223,1,223,1107,677,226,224,102,2,223,223,1006,224,389,1001,223,1,223," +
+                "1008,226,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,7,226,677,224,102,2,223,223,1005,224," +
+                "419,1001,223,1,223,1007,677,677,224,102,2,223,223,1006,224,434,1001,223,1,223,107,226,226,224,1002," +
+                "223,2,223,1005,224,449,1001,223,1,223,1008,677,226,224,102,2,223,223,1006,224,464,1001,223,1,223," +
+                "1007,677,226,224,1002,223,2,223,1005,224,479,1001,223,1,223,108,677,677,224,1002,223,2,223,1006," +
+                "224,494,1001,223,1,223,108,226,226,224,1002,223,2,223,1006,224,509,101,1,223,223,8,226,677,224,102," +
+                "2,223,223,1006,224,524,101,1,223,223,107,677,226,224,1002,223,2,223,1005,224,539,1001,223,1,223,8," +
+                "226,226,224,102,2,223,223,1005,224,554,101,1,223,223,1108,677,226,224,102,2,223,223,1006,224,569," +
+                "101,1,223,223,108,677,226,224,102,2,223,223,1006,224,584,1001,223,1,223,7,677,677,224,1002,223,2," +
+                "223,1005,224,599,101,1,223,223,1007,226,226,224,102,2,223,223,1005,224,614,1001,223,1,223,1107,226," +
+                "677,224,102,2,223,223,1006,224,629,101,1,223,223,1107,226,226,224,102,2,223,223,1005,224,644,1001," +
+                "223,1,223,1108,677,677,224,1002,223,2,223,1005,224,659,101,1,223,223,107,677,677,224,1002,223,2,223," +
+                "1006,224,674,1001,223,1,223,4,223,99,226";
+        // String input = "1002,4,3,4,33"; //
+        System.out.println(input);
+        List<Integer> integers = Arrays.asList(input.split(",")).stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
         long start = System.currentTimeMillis();
-        System.out.println("Part one: " + part1(input));
+        System.out.println("Part one: " + part1(integers));
         long one = System.currentTimeMillis();
-        System.out.println("Part two: " + part2(input));
+        System.out.println("Part two: " + part2(integers));
         long two = System.currentTimeMillis();
         System.out.println("one: " + (one - start) + "ms two: " + (two - one) + "ms");
     }
 
-    private String part1(List<String> input) {
+    private String part1(List<Integer> input) {
+        runIntProg(input);
         return "part1";
     }
 
-    private String part2(List<String> input) {
+    private void runIntProg(List<Integer> input) {
+        for (int i = 0; i < input.size(); i += 4) {
+            int op = input.get(i);
+            if (op == 99) {
+                //System.out.println("exit program i = " + i);
+                break;
+            }
+            int first = input.get(i + 1);
+            int second = input.get(i + 2);
+            int to = input.get(i + 3);
+
+            if (op == 1) {
+                input.set(to, input.get(first) + input.get(second));
+            } else if (op == 2) {
+                input.set(to, input.get(first) * input.get(second));
+            } else if (op == 3) {
+                // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
+                // For example, the instruction 3,50 would take an input value and store it at address 50.
+                //input.set()
+
+            } else {
+                System.out.println("Unknown op = " + op);
+            }
+        }
+    }
+
+    private String part2(List<Integer> input) {
         return "part2";
     }
 
-    private List<String> init() {
-        String path = "input.txt";
-        //String path = "input_test.txt";
-        if (System.getProperty("user.dir").endsWith("adventofcode")) { // executed from the proj root dir
-            path = "tmp/production/adventofcode/" + this.getClass().getSimpleName().toLowerCase() + "/" + path;
-        }
-
-        try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return Collections.emptyList();
-    }
 }
