3790a31a6ebea66703d6e6038d1640206c1a0929
Simon Kihlberg Wallström <wallstrom.simon@gmail.com>
2019-12-08 11:41:26 +0100

    d2 p1

--- 2019/day06/Day06.java
+++ 2019/day06/Day06.java
@@ -11,7 +11,7 @@ public class Day06 {
     public Day06() {
         var input = init();
         long start = System.currentTimeMillis();
-        System.out.println("Part one: " + part1(input));
+        System.out.println("Part one: " + part1(input)); // 110190
         long one = System.currentTimeMillis();
         System.out.println("Part two: " + part2(input));
         long two = System.currentTimeMillis();
@@ -19,7 +19,41 @@ public class Day06 {
     }
 
     private String part1(List<String> input) {
-        return "part1";
+        Map<String, Planet> allPlanets = new HashMap<>();
+        for (String mapRow : input) {
+            String planetString1 = mapRow.substring(0, mapRow.indexOf(')'));
+            String planetString2 = mapRow.substring(mapRow.indexOf(')') + 1);
+            //System.out.println(mapRow + " -> " + planetString1 + " ) " + planetString2);
+
+            if (!allPlanets.containsKey(planetString1)) {
+                allPlanets.put(planetString1, new Planet(planetString1));
+            }
+
+            if (!allPlanets.containsKey(planetString2)) {
+                allPlanets.put(planetString2, new Planet(planetString2));
+            }
+
+            Planet planet1 = allPlanets.get(planetString1);
+            Planet planet2 = allPlanets.get(planetString2);
+
+            planet1.orbitors.add(planet2);
+            planet2.orbits = planet1;
+        }
+
+        String rootName = "COM";
+        Planet rootPlanet = allPlanets.get(rootName);
+
+
+        return "part1: " + calcNbrOfOrbits(rootPlanet);
+    }
+
+    private int calcNbrOfOrbits(Planet rootPlanet) {
+        int count = 0;
+        for (Planet child : rootPlanet.orbitors) {
+            child.orbitCount = rootPlanet.orbitCount + 1;
+            count += child.orbitCount + calcNbrOfOrbits(child);
+        }
+        return count;
     }
 
     private String part2(List<String> input) {
@@ -34,10 +68,22 @@ public class Day06 {
         }
 
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.forEach(System.out::println);
+            return stream.collect(Collectors.toList()); //forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return Collections.emptyList();
+    }
+
+    private class Planet {
+        String name;
+        Planet orbits;
+        List<Planet> orbitors;
+        int orbitCount;
+
+        Planet(String name) {
+            this.name = name;
+            orbitors = new ArrayList<>();
+        }
     }
 }
